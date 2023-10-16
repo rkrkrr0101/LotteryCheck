82d0652127c9a79e517a7496fd98a8c243bc2f89
foko <qkrrudgh0@naver.com>
2023-10-16 22:03:21 +0900

    refactor:잘못된 변수명 수정

--- src/main/kotlin/com/lottery/check/common/Speetto.kt
+++ src/main/kotlin/com/lottery/check/common/Speetto.kt
@@ -1,5 +1,7 @@
 package com.lottery.check.common
 
-data class Speetto(val kind:SpeettoKind,val Episode:Int,
-                   val first:Int,val second:Int,val third:Int,
-                   val QuantityLeft:Int)
+data class Speetto(val kind:SpeettoKind, val episode:Int,
+                   val first:Int, val second:Int, val third:Int,
+                   val quantityLeft:Int){
+
+}
